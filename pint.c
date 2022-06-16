9166184639d10786b338f629f4852449f80d71d6
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-06-16 02:26:00 +0100

    1.pint tentative 3

--- pint.c
+++ pint.c
@@ -12,7 +12,7 @@ void  _pint(stack_t **pile, unsigned  int line_number)
 {
 	if (*pile == NULL)
 	{
-		fprintf(stderr, "L%d: can't pint, stack empty", line_number);
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		free_stack_t(*pile);
 		exit(EXIT_FAILURE);
 	}
